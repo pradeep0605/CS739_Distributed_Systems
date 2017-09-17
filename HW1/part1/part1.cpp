e526c12d257e3ed502ad768815f840893701746d
Pradeep Kashyap Ramaswamy <pradeep.kashyap@wisc.edu>
2017-09-17 16:10:00 -0500

    L1, L2, L3 cache measurements

--- HW1/part1/part1.cpp
+++ HW1/part1/part1.cpp
@@ -56,6 +56,102 @@ string clock_name(int clock_id) {
 
 //-----------------------------------------------------------------------------
 
+void print_cache_time() {
+	TimeSpec start, end;
+	uint64 min, max, sum;
+	int iterations = 0;
+	max = sum = 0;
+	min = ULLONG_MAX;
+	// Number fetched from lscpu
+	int l1_size = 32 * 1024, l2_size = 256 * 1024, l3_size = 6144 * 1024;
+	int total_size = l1_size + l2_size + l3_size;
+	// leave some space in cache for other things.
+	total_size -= (100 * 1024);
+
+	unique_ptr<char[]> buffer = make_unique<char[]>(total_size);
+	// Bring everything into cache with 0th index bytes present in l1 cache, 0th +
+	// 32k index bytes present in L3 and 0th + 32k + 256k bytes present in l3.
+
+	// hopefully all the cache hot by the end of this loop
+	for(int i = total_size -1; i >= 0; --i) {
+		buffer[i] = (i % 256);
+	}
+
+	iterations= 0;
+	max = sum = 0;
+	min = ULLONG_MAX;
+	register int reg = 0;
+	int helper = 0;
+	char *buf = buffer.get();
+	int i = 0;
+
+	for (i = 0; i < l1_size; ++i) {
+		iterations++;
+		clock_gettime(CLOCK_REALTIME, &start);
+		reg = buf[i];
+		clock_gettime(CLOCK_REALTIME, &end);
+		
+		// making sure compiler doesn't optimize things. So, using the read value
+		// from l1 cache.
+		helper += reg;
+		uint64 diff = convert_to_time(end) - convert_to_time(start);
+		sum += diff;
+		if (diff < min) { min = diff;}
+		if (diff > max) { max = diff;}
+	}
+	
+	cout << "L1 cache times :\t\t avg = "
+			<< (static_cast<double>(sum) / static_cast<double>(iterations))
+		<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
+
+	iterations= 0;
+	max = sum = 0;
+	min = ULLONG_MAX;
+	int start_idx = i;
+	for (; i < start_idx + l2_size; i += l1_size) {
+		iterations++;
+		clock_gettime(CLOCK_REALTIME, &start);
+		reg = buf[i];
+		clock_gettime(CLOCK_REALTIME, &end);
+		
+		// making sure compiler doesn't optimize things. So, using the read value
+		// from l1 cache.
+		helper += reg;
+		uint64 diff = convert_to_time(end) - convert_to_time(start);
+		sum += diff;
+		if (diff < min) { min = diff;}
+		if (diff > max) { max = diff;}
+	}
+	cout << "L2 cache times :\t\t avg = "
+			<< (static_cast<double>(sum) / static_cast<double>(iterations))
+		<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
+
+	iterations= 0;
+	max = sum = 0;
+	min = ULLONG_MAX;
+	start_idx = i;
+	for (; i < l3_size; i += l2_size) {
+		iterations++;
+		clock_gettime(CLOCK_REALTIME, &start);
+		reg = buf[i];
+		clock_gettime(CLOCK_REALTIME, &end);
+		
+		// making sure compiler doesn't optimize things. So, using the read value
+		// from l1 cache.
+		helper += reg;
+		uint64 diff = convert_to_time(end) - convert_to_time(start);
+		sum += diff;
+		if (diff < min) { min = diff;}
+		if (diff > max) { max = diff;}
+	}
+	cout << "L3 cache times :\t\t avg = "
+			<< (static_cast<double>(sum) / static_cast<double>(iterations))
+			<< " ns,\tmin = " << min << ",\tmax = " << max << ", helper = " << helper 
+			<< endl;
+}
+
+//-----------------------------------------------------------------------------
+
 void print_mutex_lock_time() {
 	mutex mtx;
 	uint64 min, max, sum;
@@ -75,7 +171,7 @@ void print_mutex_lock_time() {
 
 	cout << "Mutex lock times :\t\t avg = "
 			<< (static_cast<double>(sum) / static_cast<double>(MILLION))
-			<< "ns, min = " << min << ", max = " << max << endl;
+			<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
 }
 
 //-----------------------------------------------------------------------------
@@ -99,7 +195,7 @@ void print_mutex_unlock_time() {
 
 	cout << "Mutex unlock times :\t\t avg = "
 			<< (static_cast<double>(sum) / static_cast<double>(MILLION))
-			<< "ns, min = " << min << ", max = " << max << endl;
+			<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
 }
 
 //-----------------------------------------------------------------------------
@@ -132,9 +228,9 @@ void print_compress_uncompress_data(){
 		if (diff < min) { min = diff;}
 		if (diff > max) { max = diff;}
 	}
-	cout << "Snappy Compression Times : \t\t avg = "
+	cout << "Snappy 1k Compression Times : \t avg = "
 		<< (static_cast<double>(sum) / static_cast<double>(KILO))
-		<< "ns, min = " << min << ", max = " << max << endl;
+		<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
 
 
 	max = sum = 0;
@@ -162,9 +258,9 @@ void print_compress_uncompress_data(){
 			cout << "ERROR:not equal" << endl;
 		}
 	}
-	cout << "Snappy Decompress Times :\t\t avg = "
+	cout << "Snappy Decompress Times :\t avg = "
 		<< (static_cast<double>(sum) / static_cast<double>(iter))
-		<< "ns, min = " << min << ", max = " << max << endl;
+		<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
 }
 
 //-----------------------------------------------------------------------------
@@ -180,10 +276,14 @@ void print_main_mem_reference_time() {
 	max = sum = 0;
 	min = ULLONG_MAX;
 	
-	int n = _1_mb * 64 / sizeof(uint64);
+	// 1 GB
+	int n = _1_mb * 1024 / sizeof(uint64);
+	// skip 7 mb at once
+	int skip = (_1_mb * 7) / sizeof(uint64);
+	int iterations = 0;
 	unique_ptr<uint64[]> ptr = make_unique<uint64[]>(n);
-	for (int i = 0; i < n; i += 8) {
-		
+	for (register int i = 0; i < n; i += skip) {
+		iterations++;
 		clock_gettime(CLOCK_REALTIME, &start);
 		// Access the memory
 		ptr[i] = i;
@@ -195,22 +295,22 @@ void print_main_mem_reference_time() {
 		if (diff > max) { max = diff;}
 	}
 	cout << "Memory Access times :\t\t avg = "
-			<< (static_cast<double>(sum) / static_cast<double>(n / 8))
-			<< "ns, min = " << min << ", max = " << max << endl;
+			<< (static_cast<double>(sum) / static_cast<double>(iterations))
+			<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
 }
 
 //-----------------------------------------------------------------------------
 
 void read_1mb_from_disk() {
 	TimeSpec start, end;
-	int _1_mb = 1024 * 1024 * 1024;
+	int _1_mb = 1024 * 1024 * 1;
 	uint64 min, max, sum;
 	max = sum = 0;
 	min = ULLONG_MAX;
 	int n = 1;
 	for(int i = 0; i < n; ++i) {
 		const char *test_file =
-			static_cast<const char*>(string("/tmp/test_file").c_str());
+			static_cast<const char*>(string("test_file").c_str());
 		// system("dd if=/dev/zero of=test_file bs=1G count=1 2> /dev/null");
 			
 		int read_fd = open(test_file, 0, "r");
@@ -232,11 +332,12 @@ void read_1mb_from_disk() {
 	cout << "#Read " << (_1_mb / (1024 * 1024)) 
 			<< " MB from disk:\t\t avg = "
 			<< (sum / n)
-			<< "ns, min = " << min << ", max = " << max << endl;
+			<< " ns,\tmin = " << min << ",\tmax = " << max << endl;
 }
 
 
 int main() {
+	print_cache_time();
 	print_mutex_lock_time();
 	print_mutex_unlock_time();
 	print_main_mem_reference_time();
