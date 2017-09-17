29f6487a5269096a31352d6ffab858864d7bcc29
Sharath Prabhudeva Hiremath <shiremath@wisc.edu>
2017-09-17 13:27:42 -0500

    Including Snappy compress/Decompress in part1

--- HW1/part1/part1.cpp
+++ HW1/part1/part1.cpp
@@ -3,9 +3,13 @@
 #include <limits.h>
 #include <sys/time.h>
 #include <mutex>
+#include "../../snappy/snappy.h"
 
 #define BILLION (1000000000)
 #define MILLION (1000000)
+
+#define KILO (1000)
+
 using namespace std;
 
 typedef struct timespec TimeSpec;
@@ -92,6 +96,71 @@ void print_mutex_unlock_time() {
 }
 //-----------------------------------------------------------------------------
 
+void print_compress_uncompress_data(){
+	uint64 min, max, sum;
+	max = sum = 0;
+	min = ULLONG_MAX;
+
+	string input = "a";
+	string output;
+
+	// Generate 1k Data
+	for (int i = 0; i < 1023; ++i) {
+		string temp = " ";
+		temp[0] = ' ' + (i % 95);
+		input += temp;
+	}
+
+	for (int repeat = 0; repeat < KILO; repeat++) {
+		TimeSpec start, end;
+		// Compress
+		clock_gettime(CLOCK_REALTIME, &start);
+		snappy::Compress(input.data(), input.size(), &output);
+		clock_gettime(CLOCK_REALTIME, &end);
+		// cout << "input size:" << input.size() << " output size:"
+		//			<< output.size() << endl;
+		uint64 diff = convert_to_time(end) - convert_to_time(start);
+		sum += diff;
+		if (diff < min) { min = diff;}
+		if (diff > max) { max = diff;}
+	}
+	cout << "Snappy Compression Times : \t\t avg = "
+		<< (static_cast<double>(sum) / static_cast<double>(KILO))
+		<< "ns, min = " << min << ", max = " << max << endl;
+
+
+	max = sum = 0;
+	min = ULLONG_MAX;
+	uint64 iter = 0;
+
+	for (int repeat = 0; repeat < KILO; repeat++) {
+		TimeSpec start, end;
+		// Uncompress
+		string output_uncom;
+
+		clock_gettime(CLOCK_REALTIME, &start);
+		snappy::Uncompress(output.data(), output.size(), &output_uncom);
+		clock_gettime(CLOCK_REALTIME, &end);
+		if (input == output_uncom) {
+			// cout << "Equal" << endl;
+
+			uint64 diff = convert_to_time(end) - convert_to_time(start);
+			sum += diff;
+			iter += 1;
+			if (diff < min) { min = diff;}
+			if (diff > max) { max = diff;}
+
+		} else {
+			cout << "ERROR:not equal" << endl;
+		}
+	}
+	cout << "Snappy Decompress Times :\t\t avg = "
+		<< (static_cast<double>(sum) / static_cast<double>(iter))
+		<< "ns, min = " << min << ", max = " << max << endl;
+}
+
+//-----------------------------------------------------------------------------
+
 void print_main_mem_reference_time() {
 	
 }
@@ -102,6 +171,7 @@ int main() {
 	print_mutex_lock_time();
 	print_mutex_unlock_time();
 	print_main_mem_reference_time();
+	print_compress_uncompress_data();
 	return 0;
 
 }
